a260de012138b1b0e5480dd6762f9a9aedf29ac8
Christopher Read <Christopherread22@outlook.com>
2021-08-20 17:24:26 -0400

    Nav menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click)
+
+    const handleClick = () => setClick(!click);
+
     return (
         <>
             <nav className="navbar">
@@ -14,6 +16,9 @@ function Navbar() {
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        
+                    </ul>
                 </div>
             </nav>
         </>
