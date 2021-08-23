60f978691543012c98f5a5070f4d07148dab09a5
Christopher Read <Christopherread22@outlook.com>
2021-08-23 17:14:14 -0400

    li

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -24,8 +24,13 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                            <Link to='/services' className="nav-links" onClick={closeMobileMenu}>
                                 Services
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/products' className="nav-links" onClick={closeMobileMenu}>
+                                Products
                             </Link>
                         </li>
                     </ul>
