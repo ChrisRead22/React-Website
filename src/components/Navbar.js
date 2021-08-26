59094ebad5906fc092f1e9b3a147b7690fbbdbaf
Christopher Read <Christopherread22@outlook.com>
2021-08-26 18:55:00 -0400

    Button import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,15 @@ function Navbar() {
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
 
-    
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
 
     return (
         <>
