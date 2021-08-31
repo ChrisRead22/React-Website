1984c5654bd9c4a170a53f855d1eea26763aa14f
Christopher Read <Christopherread22@outlook.com>
2021-08-31 18:02:09 -0400

    useEffect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom';
 import './Navbar.css';
 import {Button} from './Button';
@@ -17,6 +17,8 @@ function Navbar() {
             setButton(true);
         }
     };
+
+    useEffect(())
 
     window.addEventListener('resize', showButton);
 
